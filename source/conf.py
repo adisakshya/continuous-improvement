a28998a5672c9d7c4910a96cde9d208a8da583f4
adisakshya <adisakshya98@gmail.com>
2020-12-03 13:08:29 +0530

    fix: extension-error

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -27,6 +26,9 @@ release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
+
+# Master Doc
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -44,7 +46,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -57,8 +58,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-source_parsers = {
-    '.md': CommonMarkParser
-}
-
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
